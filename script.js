bbaff1241bcb42eadc2a271af2e45dd0e54d4502
lanamads <128663000+lanamads@users.noreply.github.com>
2024-06-10 17:10:25 +1200

    Update banner animation
    Replace Vanta.js with script.js to generate stars on the banner based on mouse position. Stars animated to move from left to right

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.banner');
+
+    const coordinates = {
+        x: undefined,
+        y: undefined
+    };
+
+    const addElement = () => {
+        const newElement = document.createElement('div');
+        newElement.classList.add('star');
+
+        newElement.style.left = coordinates.x + 'px';
+        newElement.style.top = coordinates.y + 'px';
+
+        // Remove the star element after the falling animation ends
+        newElement.addEventListener('animationend', (event) => {
+            if (event.animationName === 'falling') {
+                newElement.remove();
+            }
+        });
+
+        header.appendChild(newElement);
+    }
+
+    const updateCoordinates = e => {
+        if (coordinates.x === undefined || coordinates.y === undefined) {
+            coordinates.x = e.x;
+            coordinates.y = e.y;
+            addElement();
+        }
+
+        if (Math.abs(coordinates.x - e.x) > 50 || Math.abs(coordinates.y - e.y) > 50) {
+            coordinates.x = e.x;
+            coordinates.y = e.y;
+            addElement();
+        }
+    }
+
+    header.addEventListener('mousemove', e => {
+        updateCoordinates(e);
+    });
+});
